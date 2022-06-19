dca137cddb3f3fc8b99fa108884d26ae38451cdc
MD Taseen Khan <85288261+MohammadTaseenKhan@users.noreply.github.com>
2022-06-19 13:46:50 +0600

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -7,6 +7,17 @@ $password="";
 $database="idiscuss";
 
 $conn=mysqli_connect($servername,$username,$password,$database);
+
+//MySQLi Object-Oriented
+//$conn = new mysqli($servername, $username, $password);
+
+//MySQLi Procedural
+//$conn = mysqli_connect($servername, $username, $password);
+
+//PDO
+// $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+
+
 if($conn)
 {
 	// echo "Connection done";
